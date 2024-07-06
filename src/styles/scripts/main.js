c757adca1f4275d2e679f020c927258b43d3d5d3
Denis Simplicio Pereira <denis_simplicio@hotmail.com>
2024-07-06 12:13:35 -0300

    gruntjs

---
+++ src/styles/scripts/main.js
@@ -0,0 +1,13 @@
+const { event } = require("grunt");
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento) {
+        evento.preventDefault();
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo = parseInt(numeroMaximo);
+
+        let numeroAleatorio = Math.random() * numeroMaximo;
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+    })
+})
